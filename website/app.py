9fac7bd3270780c14fb7d35b07995c884ec473bd
Sean Hart <seanhart23@gmail.com>
2023-11-15 07:53:14 -0500

    Update app.py

--- website/app.py
+++ website/app.py
@@ -137,5 +137,5 @@ def get_correlation_heatmap_data():
     return jsonify(data_compiled)
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000)
+#    app.run(host='0.0.0.0', port=5000)
 
