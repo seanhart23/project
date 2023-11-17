0dab9b72c9cddea57ab15b24066c39e06fcd629a
Sean Hart <seanhart23@gmail.com>
2023-11-17 07:59:40 -0500

    Chart speed

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,18 +1,18 @@
 const urls = [pieChartDataUrl,
-   closingChartDataUrl,
-   cumulativeReturnsChartUrl,
-   rollingBetaChartUrl,
-   roiChartUrl, 
-   heatmapChartUrl
+   // closingChartDataUrl,
+   // cumulativeReturnsChartUrl,
+   // rollingBetaChartUrl,
+   // roiChartUrl, 
+   // heatmapChartUrl
    ];
 
 Promise.all(urls.map(url => d3.json(url))).then(run);
 
 function run(dataset) {
    d3PieChart(dataset[0], dataset[1]);
-   d3ClosingChart(dataset[1]);
-   d3CumulativeReturnsChart(dataset[2]);
-   d3RollingBetaChart(dataset[3]);
-   d3RoiChart(dataset[4]);
-   d3HeatmapChart(dataset[5]);
+   // d3ClosingChart(dataset[1]);
+   // d3CumulativeReturnsChart(dataset[2]);
+   // d3RollingBetaChart(dataset[3]);
+   // d3RoiChart(dataset[4]);
+   // d3HeatmapChart(dataset[5]);
 };
