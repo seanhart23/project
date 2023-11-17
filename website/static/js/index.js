8fe71cc6e4e0e9a79ecb4ee0d0baaf82bbd869c2
Sean Hart <seanhart23@gmail.com>
2023-11-17 12:27:01 -0500

    Speed fixes

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,6 +1,6 @@
 const urls = [pieChartDataUrl,
-   // closingChartDataUrl,
-   // cumulativeReturnsChartUrl,
+   closingChartDataUrl,
+   cumulativeReturnsChartUrl,
    // rollingBetaChartUrl,
    // roiChartUrl, 
    // heatmapChartUrl
@@ -10,8 +10,8 @@ Promise.all(urls.map(url => d3.json(url))).then(run);
 
 function run(dataset) {
    d3PieChart(dataset[0], dataset[1]);
-   // d3ClosingChart(dataset[1]);
-   // d3CumulativeReturnsChart(dataset[2]);
+   d3ClosingChart(dataset[1]);
+   d3CumulativeReturnsChart(dataset[2]);
    // d3RollingBetaChart(dataset[3]);
    // d3RoiChart(dataset[4]);
    // d3HeatmapChart(dataset[5]);
