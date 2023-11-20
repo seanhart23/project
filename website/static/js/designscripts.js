a3098f8ebb74f86da6382da230c9c08753437b36
Sean Hart <seanhart23@gmail.com>
2023-11-20 16:18:48 -0500

    Update to charts

--- website/static/js/designscripts.js
+++ website/static/js/designscripts.js
@@ -3,19 +3,19 @@ var $progressBar = $('.progress-bar');
 var $alert = $('.alert');
 var $loading = $(".loading");
 
-setTimeout(function() {
-  $progressBar.css('width', '10%');
-  setTimeout(function() {
-      $progressBar.css('width', '30%');
-      setTimeout(function() {
-          $progressBar.css('width', '100%');
-          setTimeout(function() {
-            $(".loading").fadeOut();
-              $alert.css('display', 'block');
-          }, 500);
-      }, 5000);
-  }, 3000);
-}, 1000);
+// setTimeout(function() {
+//   $progressBar.css('width', '10%');
+//   setTimeout(function() {
+//       $progressBar.css('width', '30%');
+//       setTimeout(function() {
+//           $progressBar.css('width', '100%');
+//           setTimeout(function() {
+//             $(".loading").fadeOut();
+//               $alert.css('display', 'block');
+//           }, 500);
+//       }, 5000);
+//   }, 3000);
+// }, 1000);
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
