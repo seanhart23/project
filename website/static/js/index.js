17baf81ddb2efe150790871d916954babbcd40e5
Sean Hart <seanhart23@gmail.com>
2023-11-20 13:56:10 -0500

    Update index.js

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -12,18 +12,19 @@ const urlsRest = [
    ];
 
 Promise.all(urls.map(url => d3.json(url))).then(runPie);
-// Promise.all(urlsRest.map(url => d3.json(url))).then(runRest);
 
 
 function runPie(dataset) {
    d3PieChart(dataset[0], dataset[1]);
 };
 
-// function runRest(dataset) {
-//    d3ClosingChart(dataset[1]);
-//    d3CumulativeReturnsChart(dataset[2]);
-//    d3RollingBetaChart(dataset[3]);
-//    d3RoiChart(dataset[4]);
-//    d3HeatmapChart(dataset[5]);
-// };
+Promise.all(urlsRest.map(url => d3.json(url))).then(runRest);
+
+function runRest(dataset) {
+   d3ClosingChart(dataset[1]);
+   d3CumulativeReturnsChart(dataset[2]);
+   d3RollingBetaChart(dataset[3]);
+   d3RoiChart(dataset[4]);
+   d3HeatmapChart(dataset[5]);
+};
 
