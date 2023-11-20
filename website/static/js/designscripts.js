de4ed699d82361ce5625acdbc48799d739b7d620
Sean Hart <seanhart23@gmail.com>
2023-11-20 17:55:15 -0500

    Design updates

--- website/static/js/designscripts.js
+++ website/static/js/designscripts.js
@@ -17,6 +17,22 @@ var $loading = $(".loading");
 //   }, 3000);
 // }, 1000);
 
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
